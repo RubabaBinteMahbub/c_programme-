aedebd569f80f65faec4e4f1c2f93a9baab30138
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-08-15 13:50:27 +0600

    Create problem_2.c
    Problem - 2 : Checking weather the values of two integer variables are equal.
    Algorithm :
    1. Start.
    2. Read two integer variables a, b.
    3. If a==b, print "Numbers are equal".
    Else, print "Numbers are not equal".
    4. End.

---
+++ problem_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter the value of a:\n"); scanf("%d", &a);
+printf("Enter the value of b:\n");
+scanf("%d", &b);
+
+if(a==b){
+     printf("Numbers are equal.\n");
+}
+else{
+    printf("Numbers are not equal.\n");
+}
+return 0;
+} 
