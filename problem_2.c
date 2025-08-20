60a1bfb4ff27f292e049857455e8782f4fcbb765
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-08-20 11:47:21 +0600

    problem_2.c

--- problem_2.c
+++ problem_2.c
@@ -2,7 +2,8 @@
 int main()
 {
 int a,b;
-printf("Enter the value of a:\n"); scanf("%d", &a);
+printf("Enter the value of a:\n"); 
+scanf("%d", &a);
 printf("Enter the value of b:\n");
 scanf("%d", &b);
 
